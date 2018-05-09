8f2b8929ce29277967ad0ad0a76adf33cdccd8eb
Cledson <cledson@ACADEMIA.COM>
2018-05-09 12:25:59 -0300

    primeiro commit

---
+++ account.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express.Router()
+
+const init = connection =>{
+  app.get('/', async(req,res) =>{
+    // a posição 0 vira rows e a 1 vira fields, do array que vem do banco
+    const [rows,fields] = await connection.execute('select * from users')
+    console.log(rows)
+
+    res.render('home')
+  })
+
+  app.get('/new-account', (req,res) =>{
+    res.render('new-account')
+  })
+
+  app.post('/new-account',(req,res) =>{
+    res.render('new-account')
+  })
+  return app
+
+}
+
+module.exports = init //aqui ele exporta só a função
---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')//aqui é um módulo
+const app = express()// aqui está instanciando esse módulo. É como se fosse um app desse módulo
+const mysql = require('mysql2/promise')
+
+const account = require('./account')
+
+app.use(express.static('public'))//aqui é um middlewaare, onde tudo que for estático, apontará para public
+app.set('view engine', 'ejs')
+
+const init = async() =>{
+    const connection =await mysql.createConnection({
+        host:'127.0.0.1',
+        user:'root',
+        password:'',
+        database:'futiba-club'
+    })
+
+  app.use(account(connection) )
+
+  app.listen(3000, err => {   
+    console.log('Futiba Club server is running')
+  })
+}
+
+init()
+
+
